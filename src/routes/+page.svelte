d490531bc26fd0407d7c8575a8e3c4ffdeb9b4d9
hahayup <hahayupbiz@gmail.com>
2025-02-28 17:29:50 -0500

    app logo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,14 @@
-<h1>Logan Magaha</h1>
+<script>
+    import lgackey_logo from '$lib/resources/lgackey.svg'
+</script>
+
+<img src={lgackey_logo} alt="lgackey">
 
 <style>
     :global(h1) {
         text-align: center;
+    }
+    :global(body) {
+        background-color: #FDE6CA;
     }
 </style>
